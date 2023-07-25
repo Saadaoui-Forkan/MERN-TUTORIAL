418e34105cf2c1815816e5010a676e47315fd21a
unknown <forkan.saadaoui@gmail.com>
2023-07-25 06:35:51 +0100

    routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("first")
+const express = require('express')
+const dotenv = require('dotenv')
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use('/api/goals', require('./routes/goalRoute'))
+
+app.listen(port, ()=> {
+    console.log(`Server is running on port ${port}`)
+})
