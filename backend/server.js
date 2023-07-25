1f113efe9bcf2aa55247b4330c0185f4bfd4aea1
unknown <forkan.saadaoui@gmail.com>
2023-07-25 17:39:21 +0100

    connecting to database

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 const express = require('express')
-const dotenv = require('dotenv')
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
+
+connectDB()
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 app.use('/api/goals', require('./routes/goalRoute'))
+
+app.use(errorHandler)
 
 app.listen(port, ()=> {
     console.log(`Server is running on port ${port}`)
